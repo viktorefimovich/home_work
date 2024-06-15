18f8f1af1deaf17ce42d8c6807d95d12deba34fd
viktor_vershinin <viktorefimovich@bk.ru>
2024-06-15 17:47:59 +0700

    finish

--- src/widget.py
+++ src/widget.py
@@ -18,5 +18,6 @@ def get_data(date: str) -> str:
     return date_it.strftime("%d.%m.%Y")
 
 
-# if __name__ == "__main__":
-#     print(get_data("2018-07-11T02:26:18.671407"))
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 8990922113665229"))
+    print(get_data("2018-07-11T02:26:18.671407"))

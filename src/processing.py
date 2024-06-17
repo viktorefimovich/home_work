00acef2d783a899b588c7f537fdfc7ad44401e05
viktorefimovich <viktorefimovich@bk.ru>
2024-06-17 12:10:05 +0700

    add function filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,18 @@
+from typing import List, Dict
+
+
+def filter_by_state(list_dict: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция фильтрации операций по ключу state"""
+
+    new_list_dict = []
+    for dict_ in list_dict:
+        if dict_.get("state") == state:
+            new_list_dict.append(dict_)
+    return new_list_dict
+
+
+if __name__ == "__main__":
+    print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}], "EXECUTED"))

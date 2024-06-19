13ca7dcf85b1eecdbb879b6af04860f7859d8c75
viktorefimovich <viktorefimovich@bk.ru>
2024-06-19 17:35:16 +0700

    add fixes test on masks.py

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера карты"""
 
     temp_card_number = str(card_number)
-    if len(temp_card_number) < 16 or len(temp_card_number) > 16:
+    if len(temp_card_number) != 16:
         return "Неверный номер карты"
     else:
         return f"{temp_card_number[:4]} {temp_card_number[4:6]}** **** {temp_card_number[12:]}"
@@ -12,7 +12,7 @@ def get_mask_account(account_number: int) -> str:
     """Функция маскировки номера счета"""
 
     temp_account_number = str(account_number)
-    if len(temp_account_number) < 20 or len(temp_account_number) > 20:
+    if len(temp_account_number) != 20:
         return "Неверный номер счета"
     else:
         return f"**{temp_account_number[-4:]}"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,3 @@
-import pytest
-
 from src.masks import get_mask_card_number, get_mask_account
 
 

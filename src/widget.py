b2ff6c6fa2efbf3d737dfea4e7a11faeab9c2c84
viktorefimovich <viktorefimovich@bk.ru>
2024-06-19 17:35:49 +0700

    add test on widget.py

--- src/widget.py
+++ src/widget.py
@@ -16,10 +16,8 @@ def mask_account_card(input_data: str) -> Any:
 def get_data(date: str) -> str:
     """Функция преобразования даты"""
 
-    date_it = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-    return date_it.strftime("%d.%m.%Y")
-
-
-if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-    print(get_data("2018-07-11T02:26:18.671407"))
+    if len(date) != 26:
+        return "Неверный формат даты"
+    else:
+        date_it = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        return date_it.strftime("%d.%m.%Y")
---
+++ tests/test_widget.py
@@ -0,0 +1,15 @@
+import pytest
+
+from src.widget import mask_account_card, get_data
+
+
+@pytest.mark.parametrize("input_data, expected", [("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                                                  ("Счет 35383033474447895560", "Счет **5560")])
+def test_mask_account_card(input_data, expected):
+    assert mask_account_card(input_data) == expected
+
+
+def test_get_data():
+    assert get_data("2018-07-11T02:26:18.671407") == "11.07.2018"
+    assert get_data("2018-07-11") == "Неверный формат даты"
+    assert get_data("") == "Неверный формат даты"

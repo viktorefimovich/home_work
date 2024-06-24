51885b9712a88579270c451bdbc115369c74aae2
viktorefimovich <viktorefimovich@bk.ru>
2024-06-24 11:17:47 +0700

    add function transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -7,3 +7,20 @@ def filter_by_currency(data_transactions: List[Dict[str, dict]], currency: str):
     for transaction in data_transactions:
         if transaction["operationAmount"]["currency"].get("code") == currency:
             yield transaction["id"]
+
+
+# usd_transactions = filter_by_currency(transactions, "USD")
+# for _ in range(2):
+#     print(next(usd_transactions))
+
+
+def transaction_descriptions(data_transactions: List[Dict[str, dict]]):
+    """Функция, которая возвращает описание каждой операции по очереди."""
+    for transaction in data_transactions:
+        yield transaction["description"]
+
+
+# descriptions = transaction_descriptions(transactions)
+#
+# for _ in range(5):
+#     print(next(descriptions))

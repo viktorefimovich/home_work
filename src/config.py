fed09790894bb3c8f0e3df955308c201a44aa71f
viktorefimovich <viktorefimovich@bk.ru>
2024-07-11 19:48:58 +0700

    add logger utils

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-ROOT_PATH = Path(__file__).parent
+ROOT_PATH = Path(__file__).parent.parent
--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,40 @@
 import json
+import logging
 from pathlib import Path
 from typing import Any
 
-from src.config import ROOT_PATH
+from config import ROOT_PATH
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", "w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
-def get_data_transactions(file_path: Any) -> Any:
+def get_data_transactions(file_name: Path) -> Any:
     """Функция, которая принимает на вход путь до JSON-файла и возвращает список словарей с данными о транзакциях"""
 
     try:
-        with open(file_path, encoding="utf-8") as f:
+        logger.info(f"Открытие JSON-файла {file_name}")
+        with open(file_name, "r", encoding="utf-8") as f:
             try:
-                data = json.load(f)
-                return data
+                logger.info("Получение списка транзакций")
+                data_transactions = json.load(f)
+                return data_transactions
             except json.JSONDecodeError:
+                logger.error("Ошибка декодирования файла")
                 print("Ошибка декодирования файла")
                 return []
     except FileNotFoundError:
+        logger.error("Файл не найден")
         print("Файл не найден")
         return []
 
 
-path_file = Path(ROOT_PATH, "../data/operations.json")
-transactions = get_data_transactions(path_file)
-print(transactions)
+if __name__ == "__main__":
+    file_path = Path(ROOT_PATH, "data/operations.json")
+    transactions = get_data_transactions(file_path)
+    logger.info("Вывод списка транзакций")
+    print(transactions)

a22687d96846417f6e0209ee929c45b09c85b46b
viktorefimovich <viktorefimovich@bk.ru>
2024-07-29 16:49:21 +0700

    add in sorting.py function get_list_category_description

--- src/sorting.py
+++ src/sorting.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 from pathlib import Path
 
 from utils import get_data_transactions
@@ -7,6 +8,11 @@ ROOTPATH = Path(__file__).resolve().parent.parent
 
 
 def get_transactions_sort_search(list_transactions: list[dict], search_string: str) -> list[dict]:
+    """
+    Функция, которая принимает список словарей с данными о банковских операциях и строку поиска, а возвращает список
+    словарей, у которых в описании есть данная строка
+    """
+
     sort_transactions = []
     for transaction in list_transactions:
         if "description" in transaction and re.search(search_string, transaction["description"], flags=re.IGNORECASE):
@@ -19,3 +25,31 @@ if __name__ == "__main__":
     transactions = get_data_transactions(str(Path(ROOTPATH, "data/operations.json")))
 
     print(get_transactions_sort_search(transactions, search))
+
+
+def get_list_category_description(list_transactions: list[dict], list_category: list) -> dict:
+    """
+    Функция, которая принимает список словарей с данными о банковских операциях и список категорий операций,
+    а возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций в
+    каждой категории
+    """
+
+    list_category_description = []
+    for transaction in list_transactions:
+        if "description" in transaction and transaction["description"] in list_category:
+            list_category_description.append(transaction["description"])
+    sort_transaction = dict(Counter(list_category_description))
+    return sort_transaction
+
+
+if __name__ == "__main__":
+    category = [
+        "Перевод организации",
+        "Перевод с карты на карту",
+        "Перевод с карты на счет",
+        "Перевод со счета на счет",
+        "Открытие вклада",
+    ]
+    transactions = get_data_transactions(str(Path(ROOTPATH, "data/operations.json")))
+
+    print(get_list_category_description(transactions, category))
